dcc7aa0ab7372df2849adff3e5e854f0194e6463
Narudon <s5701012610122@email.kmutnb.ac.th>
2018-07-03 00:28:37 +0700

    Delete food

--- fooddetail/models.py
+++ fooddetail/models.py
@@ -3,14 +3,11 @@ from django.utils import timezone
 
 
 class Food(models.Model):
+
    food_text = models.CharField(max_length=200)
+   number_sugar = models.IntegerField(default=0)
    
    def __str__(self):
       return self.food_text
 
-class Detail(models.Model):
-   
-   sugar_text = models.CharField(max_length=200)
-   number_sugar = models.IntegerField(default=0)
-   def __str__(self):
-      return self.sugar_text
+
--- fooddetail/views.py
+++ fooddetail/views.py
@@ -1,23 +1,59 @@
 from django.shortcuts import render
 
-from .models import Food, Detail
+from .models import Food
 
 def index(request):
-   food_list = Food.objects.order_by('id')
-   sugar_list = Food.objects.order_by('id')
+   food_list = Food.objects.order_by('id').values('food_text', 'number_sugar')
+   
+   
+   #print(sugar_list)
 
-   new_food = ''
-   
-   if request.method == 'POST':
-     new_food = request.POST['new_text_food']
-     f = Food(food_text = new_food)
-   
+
    context = {
-           'food_list_to_html': food_list,
-           'sugar_list_to_html' : sugar_list
+           'food_list_to_html': food_list
+           
    }
    return render(request, 'fooddetail/index.html', context)
 
+def add_food(request):
+   
+   new_food = ''
+   new_sugar = ''
+      
+   if request.method == 'POST':
+     new_food = request.POST['new_text_food']
+     new_sugar = request.POST['new_text_sugar']
+     f = Food(food_text = new_food, number_sugar = new_sugar)
+     f.save()
+     
 
+   food_list = Food.objects.order_by('id').values('food_text', 'number_sugar')
+   
+   context = {
+           'food_list_to_html': food_list
+           
+   } 
+   
+   return render(request, 'fooddetail/index.html', context)
+
+
+def delete_food(request):
+
+    delete_food = ''
+    
+    if request.method == 'POST':
+     delete_food = request.POST['new_text_food']
+     
+     f = Food.objects.filter(food_text=delete_food)
+     f.delete()
+     
+
+    food_list = Food.objects.order_by('id').values('food_text', 'number_sugar')
+   
+    context = {
+           'food_list_to_html': food_list          
+    } 
+   
+    return render(request, 'fooddetail/index.html', context)
 
 
